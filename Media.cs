2341f2f0e8ee3d2039be36f3994f076cc3f3a650
wille <144245981+WillePersson@users.noreply.github.com>
2023-11-08 14:33:50 +0100

    Added class Bok and Media

---
+++ Media.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibrary_HyperHedgseHogs
+{
+    public abstract class Media
+    {
+        abstract void SpelaUpp();
+        abstract void VisaInfo();
+    }
+}
