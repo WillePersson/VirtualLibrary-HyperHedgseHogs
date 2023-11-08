58a78192c3a882dc2b2a6bd299c386fe5eb13fe1
Lefuden <84024382+Lefuden@users.noreply.github.com>
2023-11-08 14:54:23 +0100

    skapat klasserna Bok och Ebok med properties och metoder

--- Bok.cs
+++ Bok.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace VirtualLibrary_HyperHedgseHogs
 {
-    public class Bok
+    public abstract class Bok
     {
+        public string Titel { get; set; }
+        public string Författare { get; set; }
+        public string ISBN { get; set; }
 
+        public Bok(string titel, string författare, string isbn)
+        {
+            Titel = titel;
+            Författare = författare;
+            ISBN = isbn;
+        }
+        public virtual void SkrivUtInfo()
+        {
+            Console.WriteLine($"Titel: {Titel}\nFörfattare: {Författare}\nISBN: {ISBN}");
+        }
     }
 }
