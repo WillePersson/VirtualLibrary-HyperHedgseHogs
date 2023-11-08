51a2e1ccb288db8744874eb73373bcb7e87c9982
wille <144245981+WillePersson@users.noreply.github.com>
2023-11-08 15:01:05 +0100

    Interface klar med metoder

---
+++ Interface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibrary_HyperHedgseHogs
+{
+    interface IUtlånbar
+    {
+        void LånaUt();
+
+        void ÅterLämna();
+    }
+}
